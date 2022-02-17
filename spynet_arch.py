ba5108ca8d9795f11a7e5320216a0d30a18004d9
Anas <anassamar8@gmail.com>
2022-02-17 11:13:51 +0000

    working inference on speckle

--- spynet_arch.py
+++ spynet_arch.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import torch
 
 import getopt
@@ -25,12 +23,16 @@ arguments_strOne = './images/one.png'
 arguments_strTwo = './images/two.png'
 arguments_strOut = './out.flo'
 
-for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
-	if strOption == '--model' and strArgument != '': arguments_strModel = strArgument # which model to use, see below
-	if strOption == '--one' and strArgument != '': arguments_strOne = strArgument # path to the first frame
-	if strOption == '--two' and strArgument != '': arguments_strTwo = strArgument # path to the second frame
-	if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
-# end
+# for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
+#     if strOption == '--model' and strArgument != '':
+#         arguments_strModel = strArgument # which model to use, see below
+#     if strOption == '--one' and strArgument != '': 
+#         arguments_strOne = strArgument # path to the first frame
+#     if strOption == '--two' and strArgument != '': 
+#         arguments_strTwo = strArgument # path to the second frame
+#     if strOption == '--out' and strArgument != '': 
+#         arguments_strOut = strArgument # path to where the output should be stored
+# # end
 
 ##########################################################
 
@@ -49,9 +51,7 @@ def backwarp(tenInput, tenFlow):
 	return torch.nn.functional.grid_sample(input=tenInput, grid=(backwarp_tenGrid[str(tenFlow.shape)] + tenFlow).permute(0, 2, 3, 1), mode='bilinear', padding_mode='border', align_corners=False)
 # end
 
-##########################################################
-
-class Network(torch.nn.Module):
+class SpyNet(torch.nn.Module):
 	def __init__(self):
 		super().__init__()
 
@@ -124,58 +124,4 @@ class Network(torch.nn.Module):
 
 		return tenFlow
 	# end
-# end
-
-netNetwork = None
-
-##########################################################
-
-def estimate(tenOne, tenTwo):
-	global netNetwork
-
-	if netNetwork is None:
-		netNetwork = Network().cuda().eval()
-	# end
-
-	assert(tenOne.shape[1] == tenTwo.shape[1])
-	assert(tenOne.shape[2] == tenTwo.shape[2])
-
-	intWidth = tenOne.shape[2]
-	intHeight = tenOne.shape[1]
-
-	assert(intWidth == 1024) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
-	assert(intHeight == 416) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
-
-	tenPreprocessedOne = tenOne.cuda().view(1, 3, intHeight, intWidth)
-	tenPreprocessedTwo = tenTwo.cuda().view(1, 3, intHeight, intWidth)
-
-	intPreprocessedWidth = int(math.floor(math.ceil(intWidth / 32.0) * 32.0))
-	intPreprocessedHeight = int(math.floor(math.ceil(intHeight / 32.0) * 32.0))
-
-	tenPreprocessedOne = torch.nn.functional.interpolate(input=tenPreprocessedOne, size=(intPreprocessedHeight, intPreprocessedWidth), mode='bilinear', align_corners=False)
-	tenPreprocessedTwo = torch.nn.functional.interpolate(input=tenPreprocessedTwo, size=(intPreprocessedHeight, intPreprocessedWidth), mode='bilinear', align_corners=False)
-
-	tenFlow = torch.nn.functional.interpolate(input=netNetwork(tenPreprocessedOne, tenPreprocessedTwo), size=(intHeight, intWidth), mode='bilinear', align_corners=False)
-
-	tenFlow[:, 0, :, :] *= float(intWidth) / float(intPreprocessedWidth)
-	tenFlow[:, 1, :, :] *= float(intHeight) / float(intPreprocessedHeight)
-
-	return tenFlow[0, :, :, :].cpu()
-# end
-
-##########################################################
-
-if __name__ == '__main__':
-	tenOne = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(arguments_strOne))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
-	tenTwo = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(arguments_strTwo))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
-
-	tenOutput = estimate(tenOne, tenTwo)
-
-	objOutput = open(arguments_strOut, 'wb')
-
-	numpy.array([ 80, 73, 69, 72 ], numpy.uint8).tofile(objOutput)
-	numpy.array([ tenOutput.shape[2], tenOutput.shape[1] ], numpy.int32).tofile(objOutput)
-	numpy.array(tenOutput.numpy().transpose(1, 2, 0), numpy.float32).tofile(objOutput)
-
-	objOutput.close()
 # end
